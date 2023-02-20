1330c1c378282591b16b39a08a4ce35d9d98bf39
Gary Fagan <gary.fagan@workbuzz.com>
2023-02-20 16:18:56 +0000

    add vite config

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vitest/config'
+import react from '@vitejs/plugin-react-swc'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  esbuild: {
+    jsxInject: 'import React from \'react\'',
+  },
+  test: {
+    environment: 'jsdom',
+    globals: true,
+  },
+})
